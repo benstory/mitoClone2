bd1f97ef4c5d9e3f2dbe2e442a2a0fcbb13f0447
benstory <benstory@users.noreply.github.com>
2025-04-13 19:09:25 +0200

    Add files via upload
    update test

--- tests/testthat/test-bam.R
+++ tests/testthat/test-bam.R
@@ -4,13 +4,12 @@ context("10x file reader")
 
 test_that("correct input read from BAM", {
     input <- bam2R_10x(file = system.file("extdata", "mm10_10x.bam", package="mitoClone2"), sites="chrM:1-15000")
-    expect_length(input,6)
-    expect_named(expected=c('ZAAATCGAAAATCCG',
-                            'ZACTGGACGTCTTCG',
-                            'ZGGAAGACGCTCTTT',
-                            'ZGTGACCCCCTCTTG',
-                            'ZTCACCCCAAGCTCT',
-                            'ZTGGACGCATTATAC'),
+    expect_length(input,5)
+    expect_named(expected=c('AAATCGAAAATCCG',
+                            'ACTGGACGTCTTCG',
+                            'GGAAGACGCTCTTT',
+                            'GTGACCCCCTCTTG',
+                            'TGGACGCATTATAC'),
                  input)
     for (i in seq_along(input)) {
         expect_equal(NROW(input[[i]]), expected=15000)
